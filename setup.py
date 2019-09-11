34cc6408fc3969c250d7df101b2a92efc6b0bdb6
unai <u.alegre@utwente.nl>
2019-09-11 13:24:30 +0200

    Added basic project structure. Copied some files from skynet and renamed them.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='brainspy-algorithms',
+      version='0.0.0',
+      description='Main optimisation algorithms used for training boron-doped silicon chips both in hardware and with surrogate models.',
+      url='https://github.com/BraiNEdarwin/brainspy-algorithms',
+      author='This has adopted part of the BRAINS skynet repository code, which has been cleaned and refactored. The maintainers of the code are Hans-Christian Ruiz Euler and Unai Alegre Ibarra.',
+      author_email='u.alegre@utwente.nl',
+      license='GPL-3.0',
+      packages=find_packages(),
+      python_requires='~=3.6',
+      zip_safe=False)
