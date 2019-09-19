47f0a38aca58c1d2a9fd03f568d99a538f019180
unai <u.alegre@utwente.nl>
2019-09-19 11:32:38 +0200

    Cleaned the configurations

--- bspyalgo/utils/configs.py
+++ bspyalgo/utils/configs.py
@@ -15,29 +15,40 @@ if __name__ == '__main__':
     import numpy as np
 
     GA_EVALUATION_CONFIGS = {}
+
     GA_EVALUATION_CONFIGS['platform'] = 'simulation'
     GA_EVALUATION_CONFIGS['simulation_type'] = 'neural_network'
+    GA_EVALUATION_CONFIGS['torch_model_path'] = r'/home/unai/Documents/3-programming/boron-doped-silicon-chip-simulation/checkpoint3000_02-07-23h47m.pt'
     # platform['torch_model_path'] = r'D:\UTWENTE\PROJECTS\DARWIN\Data\Mark\MSE_n_d10w90_200ep_lr1e-3_b1024_b1b2_0.90.75.pt'
     # platform['torch_model_path'] = r'/home/hruiz/Documents/PROJECTS/DARWIN/Data_Darwin/Devices/Marks_Data/April_2019/MSE_n_d10w90_200ep_lr1e-3_b1024_b1b2_0.90.75.pt'
-    GA_EVALUATION_CONFIGS['torch_model_path'] = r'/home/unai/Documents/3-programming/boron-doped-silicon-chip-simulation/checkpoint3000_02-07-23h47m.pt'
-    GA_EVALUATION_CONFIGS['amplification'] = 10.
+
     GA_EVALUATION_CONFIGS['input_indices'] = [0, 5, 6]  # indices of NN input
     GA_EVALUATION_CONFIGS['control_indices'] = np.arange(4).tolist()  # indices of gene array
-
-    GA_CONFIGS = {}
-    GA_CONFIGS['partition'] = [5] * 5  # Partitions of population
-    # Voltage range of CVs in V
-    GA_CONFIGS['generange'] = [[-1.2, 0.6], [-1.2, 0.6], [-1.2, 0.6], [-0.7, 0.3], [-0.7, 0.3], [1, 1]]
-    # GA_CONFIGS['genes'] = len(GA_CONFIGS['generange'])    # Nr of genes
-    # GA_CONFIGS['genomes'] = sum(GA_CONFIGS['partition'])  # Nr of individuals in population
-    GA_CONFIGS['mutationrate'] = 0.1
+    GA_EVALUATION_CONFIGS['amplification'] = 10.
 
     # Parameters to define target waveforms
-    GA_CONFIGS['lengths'] = 80     # Length of data in the waveform
-    GA_CONFIGS['slopes'] = 0        # Length of ramping from one value to the next
-    # Parameters to define task
-    GA_CONFIGS['fitness_function_type'] = 'corrsig_fit'  # 'corr_fit'
+    GA_WAVEFORM_CONFIGS = {}
+    GA_WAVEFORM_CONFIGS['lengths'] = 80     # Length of data in the waveform
+    GA_WAVEFORM_CONFIGS['slopes'] = 0        # Length of ramping from one value to the next
 
+    GA_HYPERPARAMETERS = {}
+
+    # Voltage range of CVs in V
+    GA_HYPERPARAMETERS['generange'] = [[-1.2, 0.6], [-1.2, 0.6], [-1.2, 0.6], [-0.7, 0.3], [-0.7, 0.3], [1, 1]]
+    GA_HYPERPARAMETERS['partition'] = [5] * 5  # Partitions of population
+    GA_HYPERPARAMETERS['mutationrate'] = 0.1
+    GA_HYPERPARAMETERS['epochs'] = 100
+    GA_HYPERPARAMETERS['fitness_function_type'] = 'corrsig_fit'
+    GA_HYPERPARAMETERS['seed'] = None
+    # Parameters to define the fitness function and the evaluation method (hardware or simulation dependent)
+
+    GA_CONFIGS = {}
+
+    GA_CONFIGS['save_path'] = r'./tmp/output/genetic_algorithm/'
+    GA_CONFIGS['directory'] = 'OPTIMIZATION_TEST'
+
+    GA_CONFIGS['hyperparameters'] = GA_HYPERPARAMETERS
+    GA_CONFIGS['waveform_configs'] = GA_WAVEFORM_CONFIGS
     GA_CONFIGS['ga_evaluation_configs'] = GA_EVALUATION_CONFIGS    # Dictionary containing all variables for the platform
 
     save_configs(GA_CONFIGS, './configs/ga_configs.json')
