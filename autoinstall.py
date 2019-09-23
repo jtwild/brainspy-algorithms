3025a23766b943bce310be409914eb49f902afae
unai <u.alegre@utwente.nl>
2019-09-23 12:43:11 +0200

    Enable to define the directory of the configurations

--- autoinstall.py
+++ autoinstall.py
@@ -2,7 +2,7 @@ import os, time
 import subprocess as sp
 
 print('Activating the environment')
-os.system('conda activate bspyalgo')
+os.system('conda activate bspysmg')
 time.sleep(2)
 print('General setup and registering the folder into the environment path')
 os.system('python setup.py develop')
