2499bf8501b88e2307e5a0f62cb64a1688285682
unai <u.alegre@utwente.nl>
2019-09-23 11:48:59 +0200

    Improved autoinstaller

--- autoinstall.py
+++ autoinstall.py
@@ -1,13 +1,8 @@
 import os, time
 import subprocess as sp
 
-
-print('Starting autoinstall')
-print('Creating conda environment')
-cep = sp.Popen(['conda env create -f conda-env-conf.yml'], bufsize=2048, shell=True,
-    stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
-cep.wait()
-print('Activating conda environment')
+print('Activating the environment')
 os.system('conda activate bspyalgo')
-time.sleep(200)
+time.sleep(2)
+print('General setup and registering the folder into the environment path')
 os.system('python setup.py develop')
