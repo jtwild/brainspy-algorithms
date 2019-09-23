2203cb8987eda120ca168b8c687ce543752551c2
unai <u.alegre@utwente.nl>
2019-09-23 11:30:11 +0200

    Improved gitignore

--- autoinstall.py
+++ autoinstall.py
@@ -1,7 +1,13 @@
 import os, time
+import subprocess as sp
 
-os.system('conda env create -f conda-env-conf.yml')
-time.sleep(5000)
+
+print('Starting autoinstall')
+print('Creating conda environment')
+cep = sp.Popen(['conda env create -f conda-env-conf.yml'], bufsize=2048, shell=True,
+    stdin=sp.PIPE, stdout=sp.PIPE, close_fds=True)
+cep.wait()
+print('Activating conda environment')
 os.system('conda activate bspyalgo')
-time.sleep(10000)
+time.sleep(200)
 os.system('python setup.py develop')
