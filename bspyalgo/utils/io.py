included sgd in algo management; gradient descent is tested and works. Bugfix in GA: the control indices are assigned depending on input indices

bspyalgo/utils/io.py
@@ -71,12 +71,10 @@ if __name__ == '__main__':
 
     GA_EVALUATION_CONFIGS['platform'] = 'simulation'
     GA_EVALUATION_CONFIGS['simulation_type'] = 'neural_network'
-    GA_EVALUATION_CONFIGS['torch_model_path'] = r'/home/unai/Documents/3-programming/boron-doped-silicon-chip-simulation/checkpoint3000_02-07-23h47m.pt'
-    # platform['torch_model_path'] = r'D:\UTWENTE\PROJECTS\DARWIN\Data\Mark\MSE_n_d10w90_200ep_lr1e-3_b1024_b1b2_0.90.75.pt'
-    # platform['torch_model_path'] = r'/home/hruiz/Documents/PROJECTS/DARWIN/Data_Darwin/Devices/Marks_Data/April_2019/MSE_n_d10w90_200ep_lr1e-3_b1024_b1b2_0.90.75.pt'
+    GA_EVALUATION_CONFIGS['torch_model_path'] = r'tmp/NN_model/checkpoint3000_02-07-23h47m.pt'
 
     GA_EVALUATION_CONFIGS['input_indices'] = [0, 5, 6]  # indices of NN input
-    GA_EVALUATION_CONFIGS['control_indices'] = np.arange(4).tolist()  # indices of gene array
+    # TODO: See how to deal with the amplification parameters; possible source of semantic bugs
     GA_EVALUATION_CONFIGS['amplification'] = 10.
 
     # Parameters to define target waveforms
@@ -87,7 +85,7 @@ if __name__ == '__main__':
     GA_HYPERPARAMETERS = {}
 
     # Voltage range of CVs in V
-    GA_HYPERPARAMETERS['generange'] = [[-1.2, 0.6], [-1.2, 0.6], [-1.2, 0.6], [-0.7, 0.3], [-0.7, 0.3], [1, 1]]
+    GA_HYPERPARAMETERS['generange'] = [[-1.2, 0.6], [-1.2, 0.6], [-1.2, 0.6], [-0.7, 0.3], [-0.7, 0.3]]
     GA_HYPERPARAMETERS['partition'] = [5] * 5  # Partitions of population
     GA_HYPERPARAMETERS['mutationrate'] = 0.1
     GA_HYPERPARAMETERS['epochs'] = 100
@@ -104,4 +102,25 @@ if __name__ == '__main__':
     GA_CONFIGS['waveform_configs'] = GA_WAVEFORM_CONFIGS
     GA_CONFIGS['ga_evaluation_configs'] = GA_EVALUATION_CONFIGS    # Dictionary containing all variables for the platform
 
-    save_configs(GA_CONFIGS, './configs/ga_configs.json')
+    # save_configs(GA_CONFIGS, './configs/ga_configs.json')
+
+    # Make config dict for GD
+    SGD_HYPERPARAMETERS = {}
+    SGD_HYPERPARAMETERS['nr_epochs'] = 3000
+    SGD_HYPERPARAMETERS['batch_size'] = 128
+    SGD_HYPERPARAMETERS['learning_rate'] = 1e-4
+    SGD_HYPERPARAMETERS['save_interval'] = 10
+
+    SGD_MODEL_CONFIGS = {}
+    SGD_MODEL_CONFIGS['input_indices'] = [0, 1]
+    SGD_MODEL_CONFIGS['torch_model_path'] = r'tmp/NN_model/checkpoint3000_02-07-23h47m.pt'
+
+    SGD_CONFIGS = {}
+    SGD_CONFIGS['platform'] = 'simulation'
+    SGD_CONFIGS['get_network'] = 'dnpu'
+    SGD_CONFIGS['results_path'] = r'tmp/NN_test/'
+    SGD_CONFIGS['experiment_name'] = 'TEST'
+    SGD_CONFIGS['hyperparameters'] = SGD_HYPERPARAMETERS
+    SGD_CONFIGS['model_configs'] = SGD_MODEL_CONFIGS
+
+    save_configs(SGD_CONFIGS, './configs/gd/gd_configs_model_template.json')

bspyalgo/utils/pytorch.py
@@ -91,7 +91,7 @@ class TorchModel(nn.Module):
         self.info = self._info_consistency_check(state_dict['info'])
         del state_dict['info']
 
-        self._build_model(self.info)
+        self.build_model(self.info)
         self.model.load_state_dict(state_dict)
 
         if TorchUtils.get_accelerator_type() == 'cuda':
@@ -125,10 +125,11 @@ class TorchModel(nn.Module):
 
     def _get_activation(self, activation):
         if type(activation) is str:
+            print('Activation function is set as ReLU')
             return nn.ReLU()
         return activation
 
-    def _build_model(self, model_info):
+    def build_model(self, model_info):
 
         hidden_sizes = model_info['hidden_sizes']
         input_layer = nn.Linear(model_info['D_in'], hidden_sizes[0])