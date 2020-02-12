991cb5d45fd5abfd5c5df051c4a281bdcce143f9
ualegre <u.alegre@utwente.nl>
2020-02-12 13:22:41 +0100

    Fix on the adam optimiser

--- bspyalgo/algorithms/gradient/core/optim.py
+++ bspyalgo/algorithms/gradient/core/optim.py
@@ -1,11 +1,12 @@
-import torch 
+import torch
 import torch_optimizer as optim
+
 
 def get_optimizer(parameters, configs):
     if configs['optimizer'] == 'yogi':
-        return get_yogi(parameters,configs)
+        return get_yogi(parameters, configs)
     elif configs['optimizer'] == 'adam':
-        return get_adam(parameters,configs)
+        return get_adam(parameters, configs)
 
 
 def get_yogi(parameters, configs):
@@ -14,24 +15,25 @@ def get_yogi(parameters, configs):
         print("Set betas to values from the config file: ")
         print(*configs["betas"], sep=", ")
         return optim.Yogi(parameters,
-                                            lr=configs['learning_rate'],
-                                            betas=configs["betas"]
-                                            )
+                          lr=configs['learning_rate'],
+                          betas=configs["betas"]
+                          )
 
     else:
         return optim.Yogi(parameters,
-                                            lr=configs['learning_rate'])
+                          lr=configs['learning_rate'])
+
 
 def get_adam(parameters, configs):
     print('Prediction using ADAM optimizer')
     if "betas" in configs.keys():
         print("Set betas to values from the config file: ")
         print(*configs["betas"], sep=", ")
-        return torch.optim.Adam(self.processor.parameters(),
-                                            lr=configs['learning_rate'],
-                                            betas=configs["betas"]
-                                            )
+        return torch.optim.Adam(parameters,
+                                lr=configs['learning_rate'],
+                                betas=configs["betas"]
+                                )
 
     else:
-        return torch.optim.Adam(self.processor.parameters(),
-                                            lr=configs['learning_rate'])
+        return torch.optim.Adam(parameters,
+                                lr=configs['learning_rate'])
