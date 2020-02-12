89c4844319c7d5d7b57c1ca6275488e3d1dce192
ualegre <u.alegre@utwente.nl>
2020-02-12 09:53:39 +0100

    Updated installation files. Added support for yogi as a dependency.

--- autoinstall.py
+++
@@ -1,8 +0,0 @@
-import os, time
-import subprocess as sp
-
-print('Activating the environment')
-os.system('conda activate bspysmg')
-time.sleep(2)
-print('General setup and registering the folder into the environment path')
-os.system('python setup.py develop')
