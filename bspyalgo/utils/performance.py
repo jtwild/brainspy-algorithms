e1d0889661341209cb1eec2dc565bdce58cf6edf
hcruiz <hans.ruiz.euler@hotmail.com>
2020-02-26 12:12:30 +0100

    substitutes perceptron; minor details for control of plotting and verbosity. Tested.

--- bspyalgo/utils/performance.py
+++ bspyalgo/utils/performance.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 import torch
 
 
-def decision(data, targets, lrn_rate=0.007, max_iters=100, validation=False):
+def decision(data, targets, lrn_rate=0.007, max_iters=100, validation=False, verbose=True):
 
     if validation:
         n_total = len(data)
@@ -54,56 +54,32 @@ def decision(data, targets, lrn_rate=0.007, max_iters=100, validation=False):
                     w, b = [p.detach().numpy() for p in node.parameters()]
                     decision_boundary = -b / w
                     predicted_class = node(torch.tensor(data)).detach().numpy() > 0.
-        print(f'Epoch: {epoch}  Accuracy {acc}, loss: {cost.item()}')
+        if verbose:
+            print(f'Epoch: {epoch}  Accuracy {acc}, loss: {cost.item()}')
 
     return best_accuracy, predicted_class, decision_boundary
 
 
-def perceptron(input_waveform, target_waveform, tolerance=0.01, max_iter=200):
-    # Assumes that the waveform input_waveform and the target_waveform have the shape (n_total,1)
-    # Normalize the data; it is assumed that the target_waveform has binary values
-    input_waveform = (input_waveform - np.mean(input_waveform)) / np.std(input_waveform)
-    n_total = len(input_waveform)
-    weights_ = np.random.randn(2, 1)  # np.zeros((2,1))
-    inp = np.concatenate([np.ones_like(input_waveform), input_waveform], axis=1)
-    shuffle = np.random.permutation(len(inp))
-
-    x = inp[shuffle]
-    y = target_waveform[shuffle]
-
-    error = np.inf
-    j = 0
-    while (error > tolerance) and (j < max_iter):
-
-        for i in range(len(x)):
-            a = np.dot(weights_.T, x[i])
-            delta = y[i] - f(a)
-            weights_ = weights_ - delta * x[i][:, np.newaxis]
-
-        predict = np.array(list(map(f, np.dot(x, weights_))))
-        predict = predict[:, np.newaxis]
-        error = np.mean(np.abs(y - predict))
-        j += 1
-#        print('Prediction Error: ',error, ' in ', j,' iters')
-
-    buffer = np.zeros_like(y)
-    buffer[y == predict] = 1
-    n_correct = np.sum(buffer)
-    accuracy_ = n_correct / n_total
-
-#    print('Fraction of iterations used: ', j/max_iter)
-#    pdb.set_trace()
-    corrcoef = np.corrcoef(y.T, x[:, 1].T)[0, 1]
-    if accuracy_ > 0.9 and corrcoef < 0:
-        print('Weight is negative', weights_[0], ' and correlation also: ', corrcoef)
-        accuracy_ = 0.
-        print('Accuracy is set to zero!')
-
-    return accuracy_, weights_, (shuffle, predict)
-
-
-def f(x):
-    return float(x < 0)
+def perceptron(input_waveform, target_waveform, plot=None):
+    # Assumes that the input_waveform and the target_waveform have the shape (n_total,1)
+    # Normalizes the data; it is assumed that the target_waveform has binary values
+    input_waveform = (input_waveform - np.mean(input_waveform, axis=0)) / np.std(input_waveform, axis=0)
+    _accuracy, predicted_labels, threshold = decision(input_waveform, target_waveform, verbose=False)
+    if plot:
+        plt.figure()
+        plt.title(f'Accuracy: {_accuracy:.2f} %')
+        plt.plot(input_waveform, label='Norm. Waveform')
+        plt.plot(predicted_labels, '.', label='Predicted labels')
+        plt.plot(target_waveform, 'g', label='Targets')
+        plt.plot(np.arange(len(predicted_labels)),
+                 np.ones_like(predicted_labels) * threshold, 'k:', label='Threshold')
+        plt.legend()
+        if plot == 'show':
+            plt.show()
+        else:
+            plt.savefig(plot)
+            plt.close()
+    return _accuracy, predicted_labels, threshold
 
 
 def corr_coeff(x, y):
@@ -115,8 +91,8 @@ def corr_coeff(x, y):
 def accuracy(best_output, target_waveforms, mask):
     y = best_output[mask][:, np.newaxis]
     trgt = target_waveforms[mask][:, np.newaxis]
-    accuracy, _, _ = perceptron(y, trgt)
-    return accuracy
+    acc, _, _ = perceptron(y, trgt)
+    return acc
 
 
 if __name__ == '__main__':
@@ -124,18 +100,11 @@ if __name__ == '__main__':
     import pickle as pkl
 
     data_dict = pkl.load(open("tmp/input/best_output_ring_example.pkl", 'rb'))
-    data = data_dict['best_output']
-    targets = np.zeros_like(data)
-    targets[int(len(data) / 2):] = 1
-    data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
-    accuracy, predicted_labels, threshold = decision(data, targets, validation=False)
+    BEST_OUTPUT = data_dict['best_output']
+    TARGETS = np.zeros_like(BEST_OUTPUT)
+    TARGETS[int(len(BEST_OUTPUT) / 2):] = 1
+    ACCURACY, LABELS, THRESHOLD = perceptron(BEST_OUTPUT, TARGETS, plot='show')
 
-    plt.figure()
-    plt.title(f'Accuracy: {accuracy:.2f} %')
-    plt.plot(data, label='Norm. Data')
-    plt.plot(predicted_labels, '.', label='Predicted labels')
-    plt.plot(targets, 'g', label='Targets')
-    plt.plot(np.arange(len(predicted_labels)),
-             np.ones_like(predicted_labels) * threshold, 'k:', label='Threshold')
-    plt.legend()
-    plt.show()
+    MASK = np.ones_like(TARGETS, dtype=bool)
+    ACC = accuracy(BEST_OUTPUT, TARGETS, MASK)
+    print('Accuracy for best output: {ACC}')
